93f02b4a888573c147abb22a08ce42f40ae5c83e
TomasDiek <t.diekantas@gmail.com>
2023-03-30 22:37:14 +0300

    Form updates

--- src/app/components/create-listing/create-listing.component.ts
+++ src/app/components/create-listing/create-listing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api'
 import { IPropertyBase } from 'src/app/interface/IPropertyBase';
@@ -11,10 +11,10 @@ import { IPropertyBase } from 'src/app/interface/IPropertyBase';
   styleUrls: ['./create-listing.component.scss']
 })
 export class CreateListingComponent implements OnInit {
-  @ViewChild('Form') addPropertyForm!: NgForm;
   propertyTypes:Array<string>=['House','Apartment','Duplex'];
   furnishTypes:Array<string>=['Fully','Semi','Unfurnished'];
-
+  addPropertyForm!:FormGroup;
+  
   propertyView: IPropertyBase={
     id:0,
     sellRent:0,
@@ -27,17 +27,43 @@ export class CreateListingComponent implements OnInit {
     adress:'',
   };
 
-
-
-
-  constructor(private route:Router){}
+  constructor(private fb: FormBuilder, private route:Router){}
   ngOnInit(): void {
-    
+    this.CreateAddPropertyForm();
   }
   // Form:NgForm
+  CreateAddPropertyForm(){
+    this.addPropertyForm=this.fb.group({
+      BasicInformation: this.fb.group({
+        sellRent:[null,Validators.required],
+        pType:[null,Validators.required],
+        fType:[null,Validators.required],
+      }),
+      Pricing: this.fb.group({
+        price:[null,Validators.required],
+        builtArea:[null,Validators.required],
+      }),
+      Adress:this.fb.group({
+        adress:[null,Validators.required],      
+        city:[null,Validators.required],
+      }),
+      Other:this.fb.group({
+        TotalFloor: [null],
+        RTM: [null, Validators.required],
+        PossessionOn: [null],
+        AOP: [null],
+        Description: [null]
+      })
+     
+      // builtArea:[null,Validators.required],
+    })
+  }
   onSubmit(){
-    console.log('SellRent=' + this.addPropertyForm.value.basicInformation.sellRent);
+    console.log('SellRent='+this.addPropertyForm.value.BasicInformation.SellRent);
     console.log(this.addPropertyForm)
+  }
+  get BasicInformation(){
+    return this.addPropertyForm.controls.BasicInformation as FormGroup;
   }
 
 }
